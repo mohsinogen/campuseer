12669fa7a17b9d6dadb9c0a8b4480420be37514e
mohsin ansari <mohsinogen@gmail.com>
2022-03-27 15:57:55 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  
   )
 } else {
   app.get('/', (req, res) => {
